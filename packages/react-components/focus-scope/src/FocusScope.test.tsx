bc40d164332e3e510d8806ecc1ac1411aeebcf26
JingHuangSu1996 <jing.tech.tw@gmail.com>
2023-05-07 16:58:03 +0800

    [JTUI] feat: focus manager

--- packages/react-components/focus-scope/src/FocusScope.test.tsx
+++ packages/react-components/focus-scope/src/FocusScope.test.tsx
@@ -1,8 +1,85 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { FocusScope } from './';
+import { act, fireEvent, render, screen } from '@testing-library/react';
+import { FocusScope, useFocusManager } from './';
 
-test('renders FocusScope component', () => {
-  render(<FocusScope />);
-  // Add your test assertions here
+describe('FocusScope', function () {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+  afterEach(() => {
+    act(() => jest.runAllTimers());
+  });
+  describe('focus manager', function () {
+    it('should move focus forward', function () {
+      function MockButton(props: any) {
+        let focusManager = useFocusManager();
+        let onClick = () => {
+          (focusManager as any).focusNext();
+        };
+        return <button {...props} tabIndex={-1} onClick={onClick} />;
+      }
+
+      function Component() {
+        return (
+          <FocusScope>
+            <MockButton data-testid="item1">item 1</MockButton>
+            <MockButton data-testid="item2">item 2</MockButton>
+            <MockButton data-testid="item3">item 3</MockButton>
+          </FocusScope>
+        );
+      }
+
+      const { getByTestId } = render(<Component />);
+      let item1 = getByTestId('item1');
+      let item2 = getByTestId('item2');
+      let item3 = getByTestId('item3');
+
+      act(() => {
+        item1.focus();
+      });
+
+      fireEvent.click(item1);
+      expect(document.activeElement).toBe(item2);
+      fireEvent.click(item2);
+      expect(document.activeElement).toBe(item3);
+      fireEvent.click(item3);
+      expect(document.activeElement).toBe(item3);
+    });
+
+    it('should move focus backward', function () {
+      function MockButton(props: any) {
+        let focusManager = useFocusManager();
+        let onClick = () => {
+          (focusManager as any).focusPrevious();
+        };
+        return <button {...props} tabIndex={-1} onClick={onClick} />;
+      }
+
+      function Component() {
+        return (
+          <FocusScope>
+            <MockButton data-testid="item1">item 1</MockButton>
+            <MockButton data-testid="item2">item 2</MockButton>
+            <MockButton data-testid="item3">item 3</MockButton>
+          </FocusScope>
+        );
+      }
+
+      const { getByTestId } = render(<Component />);
+      let item1 = getByTestId('item1');
+      let item2 = getByTestId('item2');
+      let item3 = getByTestId('item3');
+
+      act(() => {
+        item3.focus();
+      });
+
+      fireEvent.click(item3);
+      expect(document.activeElement).toBe(item2);
+      fireEvent.click(item2);
+      expect(document.activeElement).toBe(item1);
+      fireEvent.click(item1);
+      expect(document.activeElement).toBe(item1);
+    });
+  });
 });
--- packages/react-components/focus-scope/src/FocusScope.tsx
+++ packages/react-components/focus-scope/src/FocusScope.tsx
@@ -1,12 +1,105 @@
-import React from 'react';
+import React, { ReactNode, RefObject, useContext, useEffect, useRef } from 'react';
+import { getFocusElementScope } from './utils';
 
 type FocusScopeProps = {
   /** Add a description comment for each prop. */
+  children: ReactNode;
 };
 
-export const FocusScope = React.forwardRef<HTMLDivElement, FocusScopeProps>(({}, ref) => {
-  return <div>Hello World</div>;
-});
+export interface FocusableElement extends Element, HTMLOrSVGElement {}
+
+export interface FocusManagerOptions {
+  /** The element to start searching from. The currently focused element by default. */
+  from?: Element;
+  /** Whether to only include tabbable elements, or all focusable elements. */
+  tabbable?: boolean;
+  /** Whether focus should wrap around when it reaches the end of the scope. */
+  wrap?: boolean;
+}
+export interface FocusManager {
+  /** Moves focus to the next focusable or tabbable element in the focus scope. */
+  focusNext(opts?: FocusManagerOptions): FocusableElement;
+  /** Moves focus to the previous focusable or tabbable element in the focus scope. */
+  focusPrevious(opts?: FocusManagerOptions): FocusableElement;
+}
+
+interface IFocusContext {
+  focusManager: FocusManager;
+}
+
+const FocusContext = React.createContext<IFocusContext>(null);
+
+export const useFocusManager = (): FocusManager => {
+  return useContext(FocusContext)?.focusManager;
+};
+
+export const createFocusManager = (scopeRef: RefObject<Element[]>): FocusManager => {
+  return {
+    focusNext: (opts: FocusManagerOptions) => {
+      let node = opts?.from || document.activeElement;
+      let focusable = getFocusElementScope(scopeRef.current, opts);
+
+      let nextNode = focusable.find(
+        (el) =>
+          !!(
+            (node as HTMLElement).compareDocumentPosition(el) &
+            (Node.DOCUMENT_POSITION_FOLLOWING | Node.DOCUMENT_POSITION_CONTAINED_BY)
+          ),
+      );
+
+      if (nextNode) {
+        (nextNode as HTMLElement).focus();
+      }
+
+      return nextNode;
+    },
+    focusPrevious: (opts: FocusManagerOptions) => {
+      let node = opts?.from || document.activeElement;
+      let focusable = getFocusElementScope(scopeRef.current, opts).reverse();
+
+      let previousNode = focusable.find(
+        (el) =>
+          !!(
+            (node as HTMLElement).compareDocumentPosition(el) &
+            (Node.DOCUMENT_POSITION_PRECEDING | Node.DOCUMENT_POSITION_CONTAINED_BY)
+          ),
+      );
+
+      if (previousNode) {
+        (previousNode as HTMLElement).focus();
+      }
+
+      return previousNode;
+    },
+  };
+};
+
+export const FocusScope = ({ children }: FocusScopeProps) => {
+  const startRef = useRef<HTMLSpanElement>(null);
+  const endRef = useRef<HTMLSpanElement>(null);
+  const scopeRef = useRef<Element[]>([]);
+
+  useEffect(() => {
+    let node = startRef.current?.nextSibling;
+    const nodes: Element[] = [];
+
+    while (node && node !== endRef.current) {
+      nodes.push(node as Element);
+      node = node.nextSibling;
+    }
+    scopeRef.current = nodes;
+  }, [children]);
+
+  let focusManager: FocusManager = createFocusManager(scopeRef);
+
+  return (
+    <FocusContext.Provider value={{ focusManager }}>
+      <span hidden ref={startRef} />
+      {children}
+      <span hidden ref={endRef} />
+    </FocusContext.Provider>
+  );
+};
 
 FocusScope.displayName = 'FocusScope';
 
